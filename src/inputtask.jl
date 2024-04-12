eea8b78b867c55e1136972fcc9407d4a5ace494a
David MacMahon <davidm@astro.berkeley.edu>
2024-04-11 21:43:25 -0700

    Fix indexing and iterating through input data

--- src/inputtask.jl
+++ src/inputtask.jl
@@ -1,13 +1,18 @@
-function _inputtask(data, pqout; ntpi, ntpo, fbname, fbheader)
+function _inputtask(data, pqout; ntpi, dtpi, fbname, fbheader)
+    # Sanity check dtpi vs ntpi
+    dtpi > ntpi && error("dtpi must not exceed ntpi")
+
     # Get total number of time samples from data
     ntime = size(data, 2)
     
+    startidxs = 1:dtpi:(ntime-ntpi+1)
     # Loop thrrough input time samples, sending ntpi time samples each
-    # iteration, but only advancing ntpo time samples each iteration.
-    for t in 1:ntpo:(ntime-ntpi+1)
+    # iteration, but only advancing dtpi time samples each iteration.
+    for (i,t) in enumerate(startidxs)
+        @info "processing overlapped block $i starting at time index $t"
         # Send data downstream
         produce!(pqout) do cvb
-            copyraw!(cvb, @view data[:,t:t+ntpi-1])
+            copyraw!(cvb, @view data[:,t:t+ntpi-1,:])
             coddsynchronize(cvb) # Synchronize if cvb needs it
             return (; fbname, fbheader, cvb)
         end
