7dfb82f6ef88c707cfc94c03621e913751c2d164
David MacMahon <davidm@astro.berkeley.edu>
2024-04-11 21:45:55 -0700

    Output spectra in proper Filterbank order
    The output data for each "block" of spectra was ordered as (nfpc*nchan,
    ntpo, nifs) rather than the Filterbank order (nfpc*nchan, nifs, ntpo).

--- src/outputtask.jl
+++ src/outputtask.jl
@@ -1,17 +1,22 @@
+"""
+    c2ri(c) -> ri
+
+Creates a ReinterpretArray for `c` with a leading dimension of 2 for re/im.
+If `c` has size (S1, S2, ...) then `ri` will have size (2, S1, S2, ...) with
+`real.(c) == ri[1,:,:]` and `imag.(c) == ri[2,:,:]`.
+"""
+function c2ri(c::AbstractArray{<:Complex})
+    reinterpret(reshape, real(eltype(c)), c)
+end
+
 function _outputtask(pqin)
+    local fbname
     local fbio
-    local auto_fftshift
-    local cross_fftshift
-
-    """
-        c2r(c) -> r
-
-    Creates a ReinterpretArray for c with a leading dimensino of 2 for re/im.
-    If c has size (S1, S2, ...) then r will have size (2, S1, S2, ...).
-    """
-    function c2r(c::AbstractArray{<:Complex})
-        reinterpret(reshape, real(eltype(c)), c)
-    end
+    local writebuf
+    local a11_writebufvw
+    local a22_writebufvw
+    local r12_writebufvw
+    local i12_writebufvw
 
     # While non-empty items keep coming in
     while true
@@ -21,49 +26,37 @@ function _outputtask(pqin)
 
             # Get CODDPowerBuffer from item
             cpb = item.cpb
+            nfpc, nchan, ntpo = size(cpb.autos[1])
 
-            # Open output file and allocate fftshift buffers, if needed
-            if !@isdefined fbio
-                fbio = open(item.fbname, "w")
+            # Open output file and allocate write buffer, if needed
+            if !@isdefined fbname
+                fbname = item.fbname
+                fbio = open(fbname, "w")
                 write(fbio, item.fbheader)
 
-                auto_fftshift = similar(cpb.autos[1])
-                cross_fftshift = similar(cpb.cross)
-                reim_fftshift = reinterpret(reshape,
-                    real(eltype(cross_fftshift)), cross_fftshift
-                )
+                writebuf = similar(cpb.autos[1], nfpc, nchan, 4*ntpo)
+                a11_writebufvw = @view writebuf[:,:,1:4:end]
+                a22_writebufvw = @view writebuf[:,:,2:4:end]
+                r12_writebufvw = @view writebuf[:,:,3:4:end]
+                i12_writebufvw = @view writebuf[:,:,4:4:end]
             end
 
-            for pol in cbp.autos
-                if size(pol, 1) > 1
-                    fftshift!(auto_fftshift, pol, 1)
-                    write(fbio, auto_fftshift)
-                else
-                    write(fbio, pol)
-                end
-            end
+            # Interleave spectra from autos and cross into writebuf
+            fftshift!(a11_writebufvw, cpb.autos[1], 1)
+            fftshift!(a22_writebufvw, cpb.autos[2], 1)
+            cross_reim = c2ri(cpb.cross)
+            fftshift!(r12_writebufvw, @view(cross_reim[1,:,:,:]), 1)
+            fftshift!(i12_writebufvw, @view(cross_reim[2,:,:,:]), 1)
 
-            if size(cpb.cross, 1) > 1
-                # Upchannelization, do fftshift and write
-                fftshift!(cross_fftshift, cpb.cross, 1)
-                for reim in eachslice(c2r(cross_fftshift), dims=1)
-                    write(fbio, reim)
-                end
-            else
-                # No upchannelization, write directly from cpb.cross
-                for reim in eachslice(c2r(cpb.cross), dims=1)
-                    write(fbio, reim)
-                end
-            end
+            # Write writebuf to Filterbank file
+            write(fbio, writebuf)
 
             # Return cpb for recycling
             return cpb
         end === nothing && break
     end
 
-    # Send end of input indicator downstream
-    produce!(pqout, (;))
-
     @info "coddtask done"
-    return nothing
+
+    return fbname
 end
