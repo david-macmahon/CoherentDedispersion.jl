03673c5afd1d786d8a82569fd82f16819d1c0f6c
David MacMahon <davidm@astro.berkeley.edu>
2024-04-14 12:38:35 -0700

    Cleanup rawcodd.jl

--- bin/rawcodd.jl
+++ bin/rawcodd.jl
@@ -1,67 +1,20 @@
-using CoherentDedispersion, Blio, PoolQueues, FFTW, LinearAlgebra
+using CoherentDedispersion
 
-#dir = "/datag/collate_mb/PKS_0277_2018-03-21T07:00/blc06"
-dir = "/mnt_blpc3/datax/scratch/davidm/rawcodd"
+#dir = "/datag/collate_mb/PKS_0277_2018-03-21T07:00/blc06" # Gluster
+#dir = "/mnt_blpc3/datax/scratch/davidm/rawcodd" # NFS (self mounted)
+dir = "/datax/scratch/davidm/rawcodd" # local XFS
+
 rawfile = joinpath(dir, "guppi_58198_27514_685364_J0835-4510_B3_0001.0000.raw")
 rawfiles = [rawfile]
 DMVELA = 67.771
-
-# Dispersion measure to dedisperse
-dm = DMVELA
 
 # nfpc is Number of Fine channels Per Coarse channel (set to 1 to disable)
 # nint is the Number of consecutive fine spectra to INTegrate
 #nfpc, nint = 1, 4 # Desired production values
 nfpc, nint = 16, 64 # Test values
 
-pqs, tasks = create_pipeline(rawfiles, dm; nfpc, nint)#, use_cuda=true)
+pqs, tasks = create_pipeline(rawfiles, DMVELA; nfpc, nint, outdir=".")
 
 fbname = fetch(last(tasks))
-println("saved output to $fbname")
-println("done")
-
-##
-
-# CPU-Only Pipeline:
-#
-#     Input Task (pushes overlapping blocks of RAW data)
-#     |
-#     |
-#      > CPU Voltage PQ
-#     |
-#     V
-#     CODD Task (CODD/upchan/detect)
-#     |
-#     |
-#      > CPU Power PQ
-#     |
-#     V
-#     Output Task (writes Filterbank files)
-
-# CPU/GPU Pipeline:
-#
-#     Input Task (pushes overlapping blocks of RAW data)
-#     |
-#     |
-#      > CPU Voltage PQ
-#     |
-#     V
-#     GPU Copy Task
-#                 |
-#                 |
-#                  > GPU Voltage PQ
-#                 |
-#                 V
-#                 CODD Task (CODD/upchan/detect)
-#                 |
-#                 |
-#                  > GPU Power PQ
-#                 |
-#                 V
-#     GPU Copy Task
-#     |
-#     |
-#      > CPU Power PQ
-#     |
-#     V
-#     Output Task (writes Filterbank files)
+@info "saved output to $fbname"
+@info "done"
